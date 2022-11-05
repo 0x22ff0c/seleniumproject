eff2bf8fcd53cc4df084dde30c9e8ad34d603455
0x22ff0c <markandydizon@gmail.com>
2022-11-05 22:16:38 +0800

    Added listener class to handle driver initialization prior to running test scripts in test classes and quit browser session after all test scripts have been executed

---
+++ src/main/java/utilities/Listener.java
@@ -0,0 +1,65 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener {
+	
+	WebdriverManager driverManager = null;
+	static WebDriver driver = null;
+	
+	public static WebDriver getDriver(){
+		return driver;
+	}
+	
+	@Override
+	public void onFinish(ITestContext contextFinish) {
+		
+		driverManager.quitSession();
+		
+	}
+	
+	@Override
+	public void onStart(ITestContext contextStart) {
+		
+		driverManager = new WebdriverManager("Chrome");
+		
+		driverManager.navigateToWebsite("https://www.w3schools.com/");
+
+		driver = driverManager.getDriver();
+	}
+	
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	System.out.println("Method failed with certain success percentage"+ result.getName());
+	
+	}
+	
+	@Override
+	public void onTestFailure(ITestResult result) {
+	System.out.println("Method failed"+ result.getName());
+	
+	}
+	
+	@Override
+	public void onTestSkipped(ITestResult result) {
+	System.out.println("Method skipped"+ result.getName());
+	
+	}
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		
+		System.out.println("Starting test: " + result.getTestName());
+		
+	}
+	
+	@Override
+	public void onTestSuccess(ITestResult result) {
+	System.out.println("Method passed"+ result.getName());
+	
+	}
+
+}
