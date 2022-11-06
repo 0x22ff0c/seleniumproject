f1770e8bf03ac611fb3311ab06f51a3f7d14c1a3
0x22ff0c <markandydizon@gmail.com>
2022-11-06 15:03:34 +0800

    Add custom SoftAssert to add logging

---
+++ src/main/java/utilities/SoftAssertion.java
@@ -0,0 +1,56 @@
+package utilities;
+
+import java.util.Map;
+
+import org.testng.asserts.IAssert;
+import org.testng.asserts.SoftAssert;
+import org.testng.collections.Maps;
+
+public class SoftAssertion extends SoftAssert{
+	
+	  // LinkedHashMap to preserve the order
+	  private final Map<AssertionError, IAssert<?>> m_errors = Maps.newLinkedHashMap();
+	  private static final String DEFAULT_SOFT_ASSERT_MESSAGE = "Meh";
+
+	  @Override
+	  protected void doAssert(IAssert<?> a) {
+		  System.out.println("Verify: " + a.getMessage());
+		  
+	    onBeforeAssert(a);
+	    try {
+	      a.doAssert();
+	      onAssertSuccess(a);
+	      System.out.println("Result: Passed");
+	      System.out.println("=========================================");
+	    } catch (AssertionError ex) {
+	      onAssertFailure(a, ex);
+	      System.out.println("Result: Failed");
+	      System.out.println("=========================================");
+	      m_errors.put(ex, a);
+	    } finally {
+	      onAfterAssert(a);
+	    }
+	  }
+
+	  public void assertAll() {
+	    assertAll(null);
+	  }
+
+	  public void assertAll(String message) {
+	    if (!m_errors.isEmpty()) {
+	      StringBuilder sb = new StringBuilder(null == message ? DEFAULT_SOFT_ASSERT_MESSAGE : message);
+	      boolean first = true;
+	      for (AssertionError error : m_errors.keySet()) {
+	        if (first) {
+	          first = false;
+	        } else {
+	          sb.append(",");
+	        }
+	        sb.append("\n\t");
+	        sb.append(getErrorDetails(error));
+	      }
+	      throw new AssertionError(sb.toString());
+	    }
+	  }
+
+}
