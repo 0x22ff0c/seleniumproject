6ebde4188b43f0eb393fe11e4eec326fa8185881
0x22ff0c <markandydizon@gmail.com>
2022-11-06 10:42:30 +0800

    Update BasePage class - Add method to handle exceptions - Add WebElement variable

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,28 +1,54 @@
 package pages;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class BasePage {
 
 	WebDriver driver = null;
+	WebElement element = null;
 	
 	public BasePage(WebDriver driver){
 		this.driver = driver;
 	}
 	
 	public WebElement getElementById(String id){
-		return driver.findElement(By.id(id));
+		
+		return getElement(By.id(id));
+		
 	}
 	
 	public WebElement getElementByClass(String className){
-		return driver.findElement(By.className(className));
+		
+		return getElement(By.className(className));
 		
 	}
 	
 	public WebElement getElementByXpath(String xpath){
-		return driver.findElement(By.xpath(xpath));
+		
+		return getElement(By.xpath(xpath));
+
+	}
+	
+	private WebElement getElement(By byElement){
+		
+		element = null;
+		
+		try {
+			
+			element = driver.findElement(byElement);
+			
+		} catch (NoSuchElementException noSuchElementException){
+
+			System.out.println(ExceptionUtils.getStackTrace(noSuchElementException));
+			
+		}
+	
+		return element;
+		
 	}
 	
 }
