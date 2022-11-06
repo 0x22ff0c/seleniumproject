53fd937ada934babd4a9428f4d2dd013508b9607
0x22ff0c <markandydizon@gmail.com>
2022-11-06 10:44:59 +0800

    Update TestSample class - Changed assertion from hard to soft, to prevent script from stopping once an exception occurred on one of the test steps

--- src/test/java/form/TestSample.java
+++ src/test/java/form/TestSample.java
@@ -1,20 +1,52 @@
 package form;
 
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import pages.HomePage;
 import utilities.Listener;
 
 public class TestSample {
 
-	@Test
+	@Test(testName = "Sample test name")
 	private void sampleTestMethod(){
 		
 		HomePage homePage = new HomePage(Listener.getDriver());
 		
-		Assert.assertTrue(homePage.getHomeLogo().isDisplayed());
+		SoftAssert assert1 = new SoftAssert();
+		
+		assert1.assertTrue(exceptionHandler(homePage.getHomeLogo()));
+		
+		System.out.println("Passed 1");
+		
+		assert1.assertTrue(exceptionHandler(homePage.getHomeLogo2()));
+		
+		System.out.println("Failed 2");
+		
+		assert1.assertTrue(exceptionHandler(homePage.getHomeLogo()));
+		
+		System.out.println("Passed 3");
+		
+		assert1.assertAll();
+		
+	}
+
+	private boolean exceptionHandler(WebElement element){
+		
+		boolean defaultValue = false;
+		
+		if(element == null){
+			
+			System.out.println("Unable to locate element therefore element is not displayed.");
+			
+		}else {
+			defaultValue = element.isDisplayed();
+		}
+		
+		return defaultValue;
 		
 	}
 	
 }
+
