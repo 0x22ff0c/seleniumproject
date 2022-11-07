d9e7cea21d13f982a42df437a6dff2720fac8201
0x22ff0c <markandydizon@gmail.com>
2022-11-08 00:08:47 +0800

    Update test sample class - Passed soft assertion class instead of initializing soft assert object

--- src/test/java/form/TestSample.java
+++ src/test/java/form/TestSample.java
@@ -2,7 +2,6 @@ package form;
 
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import pages.HomePage;
 import utilities.Listener;
@@ -10,12 +9,12 @@ import utilities.SoftAssertion;
 
 public class TestSample {
 
-	SoftAssert assert1 = new SoftAssertion();
-	
 	@Test(testName = "Sample test name")
 	private void sampleTestMethod(){
 		
 		HomePage homePage = new HomePage(Listener.getDriver());
+
+		SoftAssertion assert1 = Listener.getAssertion();
 		
 		assert1.assertTrue(exceptionHandler(homePage.getHomeLogo()), "Home logo is displayed");
 
