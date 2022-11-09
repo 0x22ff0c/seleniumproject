8a466c8b5a299ba7ceaf74b8908cbe34ca87466a
0x22ff0c <markandydizon@gmail.com>
2022-11-10 03:01:55 +0800

    Update TestSample class - Add priority to each test method - Rearrange test methods based on flow of verification

--- src/test/java/form/TestSample.java
+++ src/test/java/form/TestSample.java
@@ -12,35 +12,43 @@ public class TestSample extends BaseTest{
 	HomePage homePage;
 
 	Control control;
-	
-	@Test(testName = "Verify Home Page elements")
-	private void sampleTestMethod(){
-		
+
+	@Test(testName = "Verify Home page header elements", priority =  1)
+	private void testHomePageHeaderElements(){
+
 		homePage = new HomePage(Listener.getDriver());
-		
+
 		setSoftAssertion(Listener.getAssertion());
-	
+
 		verifyElementIsDisplayed(homePage.getHeader(), "Header is displayed");
 		
 		verifyElementIsDisplayed(homePage.getHomeLogo(), "Home logo is displayed");
-	
-		verifyElementIsDisplayed(homePage.getMainContent(), "Main content is displayed");
 
-		control = new Control(Listener.getDriver());
-		
-		control.scrollToElement(homePage.getFooter());
+		verifyElementIsDisplayed(homePage.getTutorialsButton(), "Tutorials button is displayed");
 
-		verifyElementIsDisplayed(homePage.getFooter(), "Footer is displayed");
+		verifyElementIsDisplayed(homePage.getTutorialsCaretDownButton(), "Tutorials caret button is displayed");
+
+		verifyElementIsDisplayed(homePage.getReferencesButton(), "References button is displayed");
+
+		verifyElementIsDisplayed(homePage.getReferencesCaretDownButton(), "References caret button is displayed");
 		
-		control.scrollToElement(homePage.getHeader());
+		verifyElementIsDisplayed(homePage.getExcercisesButton(), "Exercises button is displayed");
+		
+		verifyElementIsDisplayed(homePage.getExcercisesDownButton(), "Excercises caret button is displayed");
+		
+		verifyElementIsDisplayed(homePage.getVideosButton(), "Videos button is displayed");
 		
 		assertAll();
 		
 	}
 	
-	@Test(testName = "Verify Main page contents")
+	@Test(testName = "Verify Main page contents", priority = 2)
 	private void testMainPageContents(){
 	
+		setSoftAssertion(Listener.getAssertion());
+		
+		verifyElementIsDisplayed(homePage.getMainContent(), "Main content is displayed");
+		
 		verifyTextIsTheSame(homePage.getHeaderTextInMainContent(), "Learn to Code", "Header is displayed");
 	
 		verifyTextIsTheSame(homePage.getSubheaderTextInMainContent(), "With the world's largest web developer site.", "Subheader is displayed");
@@ -54,6 +62,23 @@ public class TestSample extends BaseTest{
 		verifyElementIsDisplayed(homePage.getWhereToBeginLink(), "\"Not Sure Where To Begin?\" link is displayed");
 		
 		assertAll();
+	}
+	
+	@Test(testName = "Verify Home Page elements", priority = 3)
+	private void sampleTestMethod(){
+
+		setSoftAssertion(Listener.getAssertion());
+		
+		control = new Control(Listener.getDriver());
+		
+		control.scrollToElement(homePage.getFooter());
+
+		verifyElementIsDisplayed(homePage.getFooter(), "Footer is displayed");
+		
+		control.scrollToElement(homePage.getHeader());
+		
+		assertAll();
+		
 	}
 
 }
