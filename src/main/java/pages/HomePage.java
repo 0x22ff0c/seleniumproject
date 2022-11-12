f0388cc85cafad4645414d5de6d6877f2192e2e1
0x22ff0c <markandydizon@gmail.com>
2022-11-12 09:28:31 +0800

    Update HomePage class
    - provide element name values to those elements with no names yet

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -12,17 +12,17 @@ public class HomePage extends BasePage{
 	//Header
 	
 	public WebElement getHeader(){
-		return getElementByXpath("//*[@title='Home']/parent::*");
+		return getElementByXpath("//*[@title='Home']/parent::*", "Header");		
 	}
 
 	//Header - menu items
 	
 	private WebElement getHeaderButton(String title){
-		return getElementByXpath(String.format("//*[@title='%s']", title));
+		return getElementByXpath(String.format("//*[@title='%s']", title), String.format("%s button", title));
 	}
 	
 	private WebElement getHeaderCaretDownButton(String title){
-		return getElementByXpath(String.format("//*[contains(@class, 'caret-down')]/parent::*[@title='%s']", title));
+		return getElementByXpath(String.format("//*[contains(@class, 'caret-down')]/parent::*[@title='%s']", title), String.format("%s caret down button", title));
 	}
 	
 	public WebElement getTutorialsButton(){
@@ -54,53 +54,53 @@ public class HomePage extends BasePage{
 	}
 	
 	public WebElement getThemeToggle(){
-		return getElementByXpath("//*[@xxtitle='Toggle Dark Code']");
+		return getElementByXpath("//*[@xxtitle='Toggle Dark Code']", "Toggle Dark Code");
 	}
 	
 	public WebElement getTranslateButton(){
-		return getElementById("nav_translate_btn");
+		return getElementById("nav_translate_btn", "Translate");
 	}
 	
 	public WebElement getSearchButton(){
-		return getElementById("nav_search_btn");
+		return getElementById("nav_search_btn", "Translate button");
 	}
 	
 	public WebElement getUpgradeButton(){
-		return getElementByXpath("//*[@title='Courses']/following-sibling::*[contains(@title,'Upgrade')]");
+		return getElementByXpath("//*[@title='Courses']/following-sibling::*[contains(@title,'Upgrade')]", "Upgrade button");
 	}
 	
 	public WebElement getCertifiedButton(){
-		return getElementById("cert_navbtn");
+		return getElementById("cert_navbtn", "Cerified button");
 	}
 	
 	public WebElement getFreeWebsiteButton(){
-		return getElementByXpath("//*[@title='Courses']/preceding-sibling::*[contains(@title, 'Get Your Own Website')]");
+		return getElementByXpath("//*[@title='Courses']/preceding-sibling::*[contains(@title, 'Get Your Own Website')]", "Free website button");
 	}
 	
 	public WebElement getLoginButton(){
-		return getElementById("w3loginbtn");
+		return getElementById("w3loginbtn", "Login button");
 	}
 	
 	public WebElement getHomeLogo(){
-		return getElementByXpath("//*[@title='Home']");
+		return getElementByXpath("//*[@title='Home']", "Home logo");
 	}
 
 	//Main Content
 	
 	public WebElement getMainContent(){
-		return getElementById("main");
+		return getElementById("main", "Main homepage content");
 	}
 	
 	public String getHeaderTextInMainContent(){
-		return getTextOfElement(getElementByXpath("//*[text()='Learn to Code']"));
+		return getTextOfElement(getElementByXpath("//*[text()='Learn to Code']",  "\"Learn to Code\" header"));
 	}
 	
 	public String getSubheaderTextInMainContent(){
-		return getTextOfElement(getElementByXpath("//*[text()=\"With the world's largest web developer site.\"]"));
+		return getTextOfElement(getElementByXpath("//*[text()=\"With the world's largest web developer site.\"]", "\"With the world's largest web developer site.\" sub header"));
 	}
 	
 	public WebElement getSearchbarField(){
-		return getElementById("search2");
+		return getElementById("search2", "Search field");
 	}
 	
 	public String getSearchbarfieldPlaceholder(){
@@ -108,15 +108,15 @@ public class HomePage extends BasePage{
 	}
 	
 	public WebElement getSearchbarButton(){
-		return getElementById("learntocode_searchbtn");
+		return getElementById("learntocode_searchbtn", "Search field button");	
 	}
 	
 	public WebElement getWhereToBeginLink(){
-		return getElementByLinkText("Not Sure Where To Begin?");
+		return getElementByLinkText("Not Sure Where To Begin?", "\"Not Sure Where To Begin?\" link");
 	}
 	
 	public WebElement getFooter(){
-		return getElementByTagName("footer");
+		return getElementByTagName("footer", "Main page footer");
 	}
 	
 }
