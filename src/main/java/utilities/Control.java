b34f3adf9b7e1c368576799e671bb9ae1bf608c3
0x22ff0c <markandydizon@gmail.com>
2022-11-15 06:45:54 +0800

    Update Control class
    - Replace BaseTest parameter with BasePage parameter in class constructor
    - Update clickButton method
    	- Invoke getElementName method from BasePage class
    	- Remove getNameOfElement method

--- src/main/java/utilities/Control.java
+++ src/main/java/utilities/Control.java
@@ -4,22 +4,23 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import pages.BasePage;
 import tests.BaseTest;
 
 public class Control{
 	
 	WebDriver driver;
 	BaseTest baseTest;
+	BasePage basePage;
 	
-	public Control(WebDriver driver, BaseTest baseTest){
+	public Control(WebDriver driver, BasePage basePage){
 		this.driver = driver;
-		this.baseTest = baseTest;
+		this.basePage = basePage;
 	}
 	
 	public void clickButton(WebElement element){
 	
-		String nameOfElement = baseTest.getNameOfElement();
-		
+		String nameOfElement = basePage.getElementName();
 		
 		System.out.println(String.format("Clicking element: %s...", nameOfElement));
 		
