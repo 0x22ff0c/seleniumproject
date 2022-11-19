84ec757b5827585badc8a9a75a8fbc00ecc22001
0x22ff0c <markandydizon@gmail.com>
2022-11-19 08:56:38 +0800

    Update WhereToStartPage class
    - Add method to get the elements and texts in the first of learning web development (HTML)

--- src/main/java/pages/homepage/maincontent/WhereToStartPage.java
+++ src/main/java/pages/homepage/maincontent/WhereToStartPage.java
@@ -23,4 +23,23 @@ public class WhereToStartPage extends BasePage{
 		return actualString.replace("\n", " ");
 	}
 	
+	//Learn HTML section
+	public WebElement getHtmlLeftButton(){
+		return getElementByXpath("//*[contains(@class, 'yellow')]/descendant::*[text()='HTML']", "HTML button");
+	}
+	
+	public String getHtmlFirstStep(){
+		return getTextOfElement(By.xpath("(//*[@title='Go To Our HTML Tutorial']/following-sibling::*)[1]"));
+	}
+	
+	public String getHtmlFirstStepAdditionalInfo(){
+		return getTextOfElement(By.xpath("(//*[@title='Go To Our HTML Tutorial']/following-sibling::*)[2]"));
+	}
+	
+	public WebElement getLearnHtmlButton(){
+		return getElementByXpath("//*[@title='Go To Our HTML Tutorial' and contains(@class, 'button')]", "Learn HTML button");
+	}
+	
+	
+	
 }
