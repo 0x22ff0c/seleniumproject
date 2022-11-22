809f21f2f084f045790663fe5667491fe3a3e25f
0x22ff0c <markandydizon@gmail.com>
2022-11-22 22:02:57 +0800

    Update WhereToStartPage
    - Add section for the footer
    	- Add method to scroll all the way down to the footer section
    	- Add methods to get the elements of the following buttons:
    		- Home
    		- Quizzes
    		- Exercises
    		- Certificates
    		- Upgrade
    		- Spaces
    		- Support
    		- Newsletter

--- src/main/java/pages/homepage/maincontent/WhereToStartPage.java
+++ src/main/java/pages/homepage/maincontent/WhereToStartPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import pages.BasePage;
+import utilities.Control;
 
 public class WhereToStartPage extends BasePage{
 
@@ -262,5 +263,43 @@ public class WhereToStartPage extends BasePage{
 	public WebElement getBackEndHomePageLink(){
 		return getElementByLinkText("Homepage");
 	}
+	
+	//Footer
+	public void scrollToWhereToStartFooter(){
+		control.scrollToElement(getElementByTagName("footer", "Where to start - footer section"));
+	}
+	
+	public WebElement getFooterHomeButton(){
+		return getElementByXpath("//footer/*[@title='Home']", "Home button");
+	}
+	
+	public WebElement getFooterQuizzesButton(){
+		return getElementByXpath("(//*[@title='Quizzes'])[1]", "Quizzes button");
+	}
+	
+	public WebElement getFooterExercisesButton(){
+		return getElementByXpath("(//*[@title='Exercises'])[1]", "Exercises button");
+	}
+	
+	public WebElement getFooterCertificatesButton(){
+		return getElementByXpath("(//*[@title='Courses and Certificates'])[1]", "Certificates button");
+	}
+	
+	public WebElement getFooterUpgradeButton(){
+		return getElementByXpath("(//*[@title='Upgrade to Improve Your Learning Experiences'])[1]", "Upgrade button");
+	}
+	
+	public WebElement getFooterSpacesButton(){
+		return getElementByXpath("(//*[@title='Get Your Own Website With W3schools Spaces'])[1]", "Spaces button");
+	}
+	
+	public WebElement getFooterSupportButton(){
+		return getElementByXpath("(//*[@title='Support'])[1]", "Support button");
+	}
+	
+	public WebElement getFooterNewsletterButton(){
+		return getElementByXpath("(//*[@title='Join Our Newsletter'])[1]", "Join Our Newsletter button");
+	}
+	
 	
 }
