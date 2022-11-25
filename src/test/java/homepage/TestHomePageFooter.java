d35824c3eacaa93456efbeba4fefec57892b2886
0x22ff0c <markandydizon@gmail.com>
2022-11-25 08:39:39 +0800

    Update TestHomePageFooter class
    - Update method: verifyFooterTexts
    	- Remove new line character when verifying: getFooterText content

--- src/test/java/homepage/TestHomePageFooter.java
+++ src/test/java/homepage/TestHomePageFooter.java
@@ -92,7 +92,7 @@ public class TestHomePageFooter {
 		
 		baseTest = new BaseTest(homePageFooter);
 		
-		baseTest.verifyTextIsTheSame(homePageFooter.getFooterText(),"W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy.\n"
+		baseTest.verifyTextIsTheSame(homePageFooter.getFooterText(),"W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. "
 				+ "Copyright 1999-2022 by Refsnes Data. All Rights Reserved.");
 	
 		baseTest.verifyElementIsDisplayed(homePageFooter.getTermsOfUseLink());
