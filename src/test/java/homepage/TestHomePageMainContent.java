ffd6c9111ce55d5b85c040a0f9a12142fb357194
0x22ff0c <markandydizon@gmail.com>
2022-11-25 08:40:34 +0800

    Update TestHomePageMainContent
    - Update method: testFooter
    	- Remove new line character when verifying: getFooterText content

--- src/test/java/homepage/TestHomePageMainContent.java
+++ src/test/java/homepage/TestHomePageMainContent.java
@@ -278,7 +278,7 @@ public class TestHomePageMainContent {
 		
 		baseTest.verifyElementIsDisplayed(footerSection.getDiscordLink());
 		
-		baseTest.verifyTextIsTheSame(footerSection.getFooterText(), "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy.\n"
+		baseTest.verifyTextIsTheSame(footerSection.getFooterText(), "W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. "
 				+ "Copyright 1999-2022 by Refsnes Data. All Rights Reserved.");
 		
 		baseTest.verifyElementIsDisplayed(footerSection.getTermsOfUseLink());
