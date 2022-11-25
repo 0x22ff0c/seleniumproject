1a63208b655234dc2298373f22cc8626a202d4c3
0x22ff0c <markandydizon@gmail.com>
2022-11-25 08:22:55 +0800

    Update FooterSection
    - Remove the following methods:
    	- scrollToWhereToStartFooter
    	- getFooterQuizzesButton
    	- getFooterExercisesButton
    	- getFooterCertificatesButton
    	- getFooterUpgradeButton
    	- getFooterSupportButton
    	- getFooterNewsletterButton
    - Update method: getFooterSpacesButton
    	- Rename to: getSpacesButton
    	- Add Override annotation

--- src/main/java/pages/homepage/maincontent/wheretostartpage/FooterSection.java
+++ src/main/java/pages/homepage/maincontent/wheretostartpage/FooterSection.java
@@ -11,61 +11,15 @@ public class FooterSection extends HomePageFooter{
 		super(driver);
 		
 	}
-	
-	//Delete method - done
-	public void scrollToWhereToStartFooter(){
-		control.scrollToElement(getElementByTagName("footer", "Where to start - footer section"));
-	}
-	
+
 	public WebElement getFooterHomeButton(){
 		return getElementByXpath("//footer/*[@title='Home']", "Home button");
 	}
-	
-	//Delete method - done
-	public WebElement getFooterQuizzesButton(){
-		
-		return getButtonElement("Quizzes");
-		
-	}
-	
-	//Delete method - done
-	public WebElement getFooterExercisesButton(){
-		
-		return getButtonElement("Exercises");
-		
-	}
-	
-	//Delete method - done
-	public WebElement getFooterCertificatesButton(){
-		
-		return getButtonElement("Courses and Certificates", "Certificates");
-		
-	}
-	
-	//Delete method - done
-	public WebElement getFooterUpgradeButton(){
-		
-		return getButtonElement("Upgrade to Improve Your Learning Experiences", "Upgrade");
-	}
-	
-	//Override method: getSpaces button
-	public WebElement getFooterSpacesButton(){
+
+	@Override
+	public WebElement getSpacesButton(){
 		
 		return getButtonElement("Get Your Own Website With W3schools Spaces", "Spaces");
-		
-	}
-	
-	//Delete method - done
-	public WebElement getFooterSupportButton(){
-		
-		return getButtonElement("Support");
-		
-	}
-	
-	//Delete method - done
-	public WebElement getFooterNewsletterButton(){
-		
-		return getButtonElement("Join Our Newsletter");
 		
 	}
 	
