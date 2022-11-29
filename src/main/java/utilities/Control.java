3a207294a1824e024ee14e2e1e2fa746bc14f4ca
0x22ff0c <markandydizon@gmail.com>
2022-11-30 07:39:24 +0800

    Update Control class
    - Add method: waitForElmentToBeDisplayedInPage
    - Instantiate wait object in Control constructor

--- src/main/java/utilities/Control.java
+++ src/main/java/utilities/Control.java
@@ -1,10 +1,15 @@
 package utilities;
 
+import java.time.Duration;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pages.BasePage;
 import tests.BaseTest;
@@ -16,16 +21,24 @@ public class Control{
 	BasePage basePage;
 	String nameOfElement;
 	Logger logger = LogManager.getLogger();
+	WebDriverWait wait;
 	
 	public Control(WebDriver driver, BasePage basePage){
 		this.driver = driver;
 		this.basePage = basePage;
+		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 	}
 	
 	public Control(WebDriver driver){
 		this.driver = driver;
 	}
 
+	public void waitForElmentToBeDisplayedInPage(By locator){
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		
+	}
+	
 	private void logBeforeAction(String message){
 		
 		LogUtility.logInfo(message + "...");
@@ -69,7 +82,7 @@ public class Control{
 		logAfterAction(String.format("Filled-in field with value: %s", valueToFillInTheField));
 		
 	}
-	
+		
 	public void scrollToElement(WebElement element){
 		
 		nameOfElement = basePage.getElementName();
