ae9d980a82750d68c6c24924b812fdaea8ab5e0d
0x22ff0c <markandydizon@gmail.com>
2022-12-04 07:27:26 +0800

    Update HeaderEnum
    - Add the following constants:
    	- TOGGLE_DARK_CODE
    	- TRANSLATE_BUTTON
    	- SEARCH_BUTTON
    	- UPGRADE_BUTTON
    	- CERTIFIED_BUTTON
    	- FREE_WEBSITE_BUTTON
    	- LOGIN_BUTTON
    	- HOME_LOGO

--- src/main/java/enums/homepage/HeaderEnum.java
+++ src/main/java/enums/homepage/HeaderEnum.java
@@ -10,8 +10,24 @@ public enum HeaderEnum {
 	
 	EXERCISES_MENU_BUTTON("Exercises"),
 	
-	VIDEO_TUTORIALS_BUTTON("Video Tutorials");
+	VIDEO_TUTORIALS_BUTTON("Video Tutorials"),
 		
+	TOGGLE_DARK_CODE("Toggle Dark Code"),
+	
+	TRANSLATE_BUTTON("Translate"),
+	
+	SEARCH_BUTTON("Search"),
+	
+	UPGRADE_BUTTON("Upgrade"),
+	
+	CERTIFIED_BUTTON("Certified"),
+	
+	FREE_WEBSITE_BUTTON("Free Website"),
+	
+	LOGIN_BUTTON("Login"),
+	
+	HOME_LOGO("Home logo");
+	
 	private String elementName;
 	
 	private HeaderEnum(String elementName){
