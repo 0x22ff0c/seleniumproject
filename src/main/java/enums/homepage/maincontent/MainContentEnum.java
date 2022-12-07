8d591fb9a716934a26b8d1e2bcf7a89bf1d18bcc
0x22ff0c <markandydizon@gmail.com>
2022-12-08 01:04:58 +0800

    Update MainContentEnum
    - Add constants for the 1st section of homepage main content

--- src/main/java/enums/homepage/maincontent/MainContentEnum.java
+++ src/main/java/enums/homepage/maincontent/MainContentEnum.java
@@ -2,4 +2,27 @@ package enums.homepage.maincontent;
 
 public enum MainContentEnum {
 
+	MAIN_CONTENT_SECTION("Main homepage content"),
+	
+	LEARN_TO_CODE_HEADER("Learn to Code"),
+	
+	LEARN_TO_CODE_SUBHEADER("With the world's largest web developer site."),
+	
+	SEARCH_FIELD("Search field"),
+	
+	SEARCH_FIELD_BUTTON("Search field button"),
+	
+	NOT_SURE_WHERE_TO_BEGIN_LINK("Not Sure Where To Begin?");
+	
+	private String elementName; 
+	
+	private MainContentEnum(String elementName){
+		this.elementName = elementName;
+	}
+	
+	@Override
+	public String toString(){
+		return elementName;
+	}
+	
 }
