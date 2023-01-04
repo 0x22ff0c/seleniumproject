0973b53abff9362e1c7bc526f21093d7f9372bcf
0x22ff0c <markandydizon@gmail.com>
2023-01-05 06:57:15 +0800

    Updat TestHomePageFooter
    - update method: verifyFooterTexts
    	- invoke: getYear from HomePageFooter to set the year since it's dynamic

--- src/test/java/homepage/TestHomePageFooter.java
+++ src/test/java/homepage/TestHomePageFooter.java
@@ -78,8 +78,8 @@ public class TestHomePageFooter {
 		
 		baseTest = new BaseTest(homePageFooter);
 		
-		baseTest.verifyTextIsTheSame(homePageFooter.getFooterText(),"W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. "
-				+ "Copyright 1999-2022 by Refsnes Data. All Rights Reserved.");
+		baseTest.verifyTextIsTheSame(homePageFooter.getFooterText(),String.format("W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy. "
+				+ "Copyright 1999-%s by Refsnes Data. All Rights Reserved.", homePageFooter.getYear()));
 	
 		baseTest.verifyElementIsDisplayed(homePageFooter.getTermsOfUseLink());
 		
